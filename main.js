d129ef7095c312cd6e18bc6069071a09cfc1db24
rajatruikar <rajatruikar12@gmail.com>
2024-02-05 18:26:20 +0530

    5/2/24

--- main.js
+++ main.js
@@ -52,11 +52,51 @@ function user() {
     flavour = selectedValues.join(", ");
     Gender = getSelectedRadioValue("Gender");
     Age = getSelectedRadioValue("Age");
-    console.log(Name.value + "  " + Gender + "  " + Age + "  " + ContactNo.value + "  " + flavour.toString());
-    if(Name.value == "" || Gender == undefined || Age == undefined || ContactNo == ""){
+    // console.log(Name.value + "  " + Gender + "  " + Age + "  " + ContactNo.value + "  " + flavour.toString());
+    if (Name.value == "" || Gender == undefined || Age == undefined || ContactNo == "") {
         alert('Please fill all the fields');
     }
-    else{
-        window.location ="thanks.html"   
+    else {
+        const data = {
+            Name: Name.value,
+            Gender: Gender,
+            Age: Age,
+            ContactNo: ContactNo.value,
+            Flavour: flavour.toString()
+        }
+
+        console.log(Name.value + "  " + Gender + "  " + Age + "  " + ContactNo.value + "  " + flavour.toString());
+
+        fetch('http://localhost:3000/api/hello', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then(response => response.json())
+            .then(responseData => {
+                // Display the response data in the console or on the page
+                console.log(responseData);
+                // You can also display it on the page, for example:
+                // document.body.innerHTML = JSON.stringify(responseData);
+            })
+            .catch(error => console.error('Error:', error));
     }
+    // window.location = "thanks.html"
 }
+
+// let Option = {
+//     method: 'POST',
+//     body: JSON.stringify({
+//       title: 'Rajat',
+//       body: 'Ruikar',
+//       userId: 1,
+//     }),
+//     headers: {
+//       'Content-type': 'application/json; charset=UTF-8',
+//     },
+//   }
+//       fetch('https://jsonplaceholder.typicode.com/posts', Option)
+//         .then((response) => response.json())
+//         .then((json) => console.log(json));
